9366ce3063fcb6a02aa6c22486146693230b6f06
Bartu Tiryakioglu <bartu@gullivers.games>
2022-08-25 17:32:36 +0300

    Initialize Action

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const fetchedVersion = core.getInput('version');
+  console.log(`Fetched Version is ${fetchedVersion}!`);
+  var versionWithoutZeros = "";
+  for (char of fetchedVersion){
+    if (char != '0'){
+        versionWithoutZeros += char;
+    }
+  }
+  const output = str + "-v" + versionWithoutZeros;
+  core.setOutput("new-version", output);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
